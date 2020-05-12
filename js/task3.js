2451d9cccebe9b66f892f5bccef7cc8f5c48401d
Denis <dzianis.pipko@gmail.com>
2020-05-12 15:13:00 +0300

    commit task3

---
+++ js/task3.js
@@ -0,0 +1,6 @@
+list = (strNumbers) => {
+   let strNumbersArray = strNumbers.split(' ').sort((a, b) => a - b);
+   return `${strNumbersArray.pop()} ${strNumbersArray.shift()}`;
+}
+
+list("4 5 29 54 4 0 -123 666 -64 1 -3 6 -6");
