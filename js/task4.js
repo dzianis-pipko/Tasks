1737f58acd9ded090242c994537aebe7d5bc3f0b
Denis <dzianis.pipko@gmail.com>
2020-05-12 23:22:59 +0300

    commit task4-part2

--- js/task4.js
+++ js/task4.js
@@ -4,8 +4,24 @@ accum = (str) => {
       .map((item, index) => (index == 0) ? item : item.repeat(index + 1))
       .map(item => item[0].toUpperCase() + item.slice(1))
       .join('-');
-}
+};
 
 accum("abcd"); //-> "A-Bb-Ccc-Dddd"
 accum("RqaEzty"); //-> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt"); //-> "C-Ww-Aaa-Tttt"
+
+
+// Вариант 2
+
+accumTwo = (str) => {
+   let arr = [];
+   str.toLowerCase()
+      .split('')
+      .reduce((iterator, item) => (iterator == 0) ? arr.push(item) : arr.push(item.repeat(iterator + 1)), 0);
+   return arr.map(item => item[0].toUpperCase() + item.slice(1)).join('-');
+
+};
+
+accumTwo("abcd"); //-> "A-Bb-Ccc-Dddd"
+accumTwo("RqaEzty"); //-> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accumTwo("cwAt"); //-> "C-Ww-Aaa-Tttt"
