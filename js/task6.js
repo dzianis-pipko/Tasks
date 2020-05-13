46f59434082aa31c154043d7fd269f5cc488d60e
Denis <dzianis.pipko@gmail.com>
2020-05-13 18:05:17 +0300

    commit task6

---
+++ js/task6.js
@@ -0,0 +1,15 @@
+printer_error = (str) => {
+   let strArr = str.split('').map((item) => (item > 'm') ? false : true);
+   let arrErrors = [];
+   for (let key of strArr) {
+      if (key == false) arrErrors.push(key);
+   }
+   return `${arrErrors.length}/${strArr.length}`;
+};
+
+printer_error('aaaxbbbbyyhwawiwjjjwwm');
+printer_error('aaabbbbhaijjjm');
+printer_error('aaabbbbhaijjjmnnnpqrstuzzzm');
+
+// abcdefghijklm
+// nopqrstuvwxyz
