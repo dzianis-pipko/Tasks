c41ab64bf764359120aab6955e4a2a854ed96a24
Denis <dzianis.pipko@gmail.com>
2020-05-14 17:37:28 +0300

    ff

---
+++ js/task10.js
@@ -0,0 +1,4 @@
+findNeedle = (arr) => {
+   return `нашел иглу в положении ${arr.indexOf('needle')}`;
+};
+console.log(findNeedle(["bla", "wekmvever", "needle", "bntyn", "qwd", "qfverv", "rtbrtb"]));
